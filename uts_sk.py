f72c873578209bb948b8c9cab41c3c794eb88f4a
Louis <corneliuslouis3112@gmail.com>
2024-11-29 13:05:47 +0700

    create menu

---
+++ uts_sk.py
@@ -0,0 +1,33 @@
+import numpy as nu
+
+def main():
+    print("================ Kelompok 7 ==================")
+    print("| 1. Rayhan Hendra Atmadja     075 - TI 23 C |")
+    print("| 2. Adriano Emmanuel          082 - TI 23 C |")
+    print("| 3. Cornelius Louis Nathan    085 - TI 23 C |")
+    print("==============================================")
+
+    while True:
+        print("\n=== Implementasi Sains Kom Semester 3===")
+        print("1. Systems of Linear Equations") # Sistem Persamaan Linear 
+            # nanti terbagi menjadi 2, cara triangularisasi dan eliminasi gauss
+        print("2. Implementation of Matrix")
+            # ini bisa cuma ngeprint sifat" matrix, atau menampilkan penerapan matrix nya
+        print("3. Invertible Matrik") # Invers Matrix
+        print("4. LU Decomposition")
+        print("5. Linear Equations")  # Persamaan linear
+            # sebenarnya aku bingung mau implemen ini, karena pptnya gk dikasih ama ibunya   
+        print("6. Non Linear Equations")
+            # ada 2 metode, metode biseksi (metode tertutup) dan newton rhapson (metode terbuka) (pilih salah satu aja gak seh?)
+        print("8. Interpolation") # Interpolasi
+            # ada 3, interpolasi linear, interpolasi kuadrat/non linear, polinom newton (pilih salah satu aja gak seh?)
+        print("7. Basic Simulation")
+            # sejauh ini, kalau simulasi..  paling mentok bisa nerapin Linear Congruential Generators (LCG) (PPT Slide 18)
+            # LCG itu teori kalau misalkan hasil perhitungannya melebihi mod itu, maka hasil perhitungannya akan kembali seperti semula
+        print("8. Iterasi Theory") # Teori iterasi 
+            # iterasi jacobi dan gauss-seidler
+        print("9. Monte-Carlo Simulation") 
+        print("10. Markov Chain")
+        break
+
+if __name__ == "__main__" : main() 
